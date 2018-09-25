77e27d454502705029ce4ebe6e96af7c2f94a7bf
Dev User <contact@patfarley.org>
2018-09-25 19:29:52 +0900

    bare bones python implementaion for inference

---
+++ xor.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+class Xor:
+
+	def predict(self, X):
+		h1 = self.append_bias(X) @ [[0, -1], [1 , 1], [1, 1]]
+		a1 = self.relu(h1)
+		o1 = self.append_bias(a1) @ [[0], [1], [-2]]
+
+		return o1
+
+	def relu(self, X):
+		return np.maximum(0, X)
+
+	def append_bias(self, X):
+		return np.hstack((np.ones(len(X)).reshape(-1,1), X))
+
+if __name__ == "__main__":
+	import unittest
+
+	class Test(unittest.TestCase):
+
+		def test_minimal_case(self):
+			np.testing.assert_array_equal([[1]], Xor().predict([[0, 1]]))
+
+		def test_minimal_exhaustive(self):
+			actual = Xor().predict([[0, 0], [0, 1], [1, 0], [1, 1]])
+			expected = [[0], [1], [1], [0]]
+			np.testing.assert_array_equal(expected, actual)
+
+	unittest.main()
