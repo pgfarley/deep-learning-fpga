9ccce1f05bae161a6d60925e37a76e04c594e73b
Dev User <contact@patfarley.org>
2018-09-27 13:49:30 +0900

    relu as a function. Move some stuff to combinational logic

--- xor_nn.v
+++ xor_nn.v
@@ -1,7 +1,4 @@
 //TODO  bus protocol
-//TODO  matrix input, vector output
-//TODO  FSM
-//TODO  move some logic to combinational?
 //TODO  Allow weights to be passed in
 //TODO  reset_m support
 //TODO dynamic layer sizes
@@ -16,9 +13,19 @@ module xor_nn(	input clk,
 wire[2:0] x = {input_data, 1'b1};
 
 reg signed [7:0]  w1 [2:0][1:0];
-reg signed [7:0]  h1 [2:0][0:0];
-reg [7:0] a1 [2:0][0:0];
+wire [7:0] a1 [2:0][0:0];
 reg signed [7:0] w2 [2:0][0:0];
+
+function relu;
+	input signed [7:0] value;
+	begin
+		relu = value & ~value[7];
+	end
+endfunction
+	
+assign a1[0][0] = 1;
+assign a1[1][0] = relu(x[0] * w1[0][0] + x[1] * w1[1][0] + x[2] * w1[2][0]);
+assign a1[2][0] = relu(x[0] * w1[0][1] + x[1] * w1[1][1] + x[2] * w1[2][1]);
 
 always @ (posedge clk)
 begin
@@ -33,20 +40,6 @@ begin
 	w2[1][0] <= 1;
 	w2[2][0] <= -2;
 
-	h1[0][0] <= 1;
-	h1[1][0] <= x[0] * w1[0][0] + x[1] * w1[1][0] + x[2] * w1[2][0];
-	h1[2][0] <= x[0] * w1[0][1] + x[1] * w1[1][1] + x[2] * w1[2][1];
-
-	a1[0][0] <= 1;
-	if (h1[1][0] < 0)
-		a1[1][0] <= 0;
-	else
-		a1[1][0] <= h1[1][0];
-
-	if (h1[2][0] < 0)
-		a1[2][0] <= 0;
-	else
-		a1[2][0] <= h1[2][0];
 
 	prediction_data <= a1[0][0] * w2[0][0] + a1[1][0] * w2[1][0] + a1[2][0] * w2[2][0];
 
