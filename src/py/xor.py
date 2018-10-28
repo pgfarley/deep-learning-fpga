71e9ac5d8c576d1ded518c97840431412b92d96f
Dev User <contact@patfarley.org>
2018-10-28 16:52:52 +0900

    pep8

--- src/py/xor.py
+++ src/py/xor.py
@@ -1,36 +1,36 @@
 import numpy as np
 
+
 class Xor:
 
-	def __init__(self):
-		self.w1 = [[0, -1], [1 , 1], [1, 1]]
-		self.w2 = [[0], [1], [-2]]
+    def __init__(self):
+        self.w1 = [[0, -1], [1, 1], [1, 1]]
+        self.w2 = [[0], [1], [-2]]
 
+    def predict(self, X):
+        h1 = self.append_bias(X) @ self.w1
+        a1 = self.relu(h1)
+        o1 = self.append_bias(a1) @ self.w2
 
-	def predict(self, X):
-		h1 = self.append_bias(X) @ self.w1
-		a1 = self.relu(h1)
-		o1 = self.append_bias(a1) @ self.w2
+        return o1
 
-		return o1
+    def relu(self, X):
+        return np.maximum(0, X)
 
-	def relu(self, X):
-		return np.maximum(0, X)
-
-	def append_bias(self, X):
-		return np.hstack((np.ones(len(X)).reshape(-1,1), X))
+    def append_bias(self, X):
+        return np.hstack((np.ones(len(X)).reshape(-1, 1), X))
 
 if __name__ == "__main__":
-	import unittest
+    import unittest
 
-	class Test(unittest.TestCase):
+    class Test(unittest.TestCase):
 
-		def test_minimal_case(self):
-			np.testing.assert_array_equal([[1]], Xor().predict([[0, 1]]))
+        def test_minimal_case(self):
+            np.testing.assert_array_equal([[1]], Xor().predict([[0, 1]]))
 
-		def test_minimal_exhaustive(self):
-			actual = Xor().predict([[0, 0], [0, 1], [1, 0], [1, 1]])
-			expected = [[0], [1], [1], [0]]
-			np.testing.assert_array_equal(expected, actual)
+        def test_minimal_exhaustive(self):
+            actual = Xor().predict([[0, 0], [0, 1], [1, 0], [1, 1]])
+            expected = [[0], [1], [1], [0]]
+            np.testing.assert_array_equal(expected, actual)
 
-	unittest.main()
+    unittest.main()
